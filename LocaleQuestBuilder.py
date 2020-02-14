9bc2952bd2b8abfe7f907c93a40c3dbe5d7cde67
Owsap <owsap@mail.com>
2020-02-14 18:49:16 -0100

    Add files via upload

---
+++ LocaleQuestBuilder.py
@@ -0,0 +1,44 @@
+__author__ = "Owsap"
+__copyright__ = "Copyright 2020, Owsap Productions"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+import os
+import sys
+import logging
+
+LOG_FILE_NAME = "LocaleQuestBuilder.log" # Log file name
+LOCALE_QUEST_FILE_NAME = "locale_quest.txt" # Locale quest file name
+
+logging.basicConfig(filename = "log/" + LOG_FILE_NAME, level = logging.DEBUG, format = '%(asctime)s %(message)s', datefmt = '%d/%m/%Y %H:%M:%S')
+
+def GetLocaleQuestFile(locale):
+	return "locale/%s/%s" % (locale, LOCALE_QUEST_FILE_NAME)
+
+def TranslateLocaleQuest(locale):
+	localeQuestOutput = "translate_%s.lua" % locale
+	if os.path.exists(localeQuestOutput):
+		os.remove(localeQuestOutput)
+
+	if not os.path.exists(GetLocaleQuestFile(locale)):
+		print "%s not found." % GetLocaleQuestFile(locale)
+		logging.warning("%s not found." % GetLocaleQuestFile(locale))
+		return
+
+	fileOutput = open(localeQuestOutput, 'a')
+	for line in open(GetLocaleQuestFile(locale), 'r'):
+		line = line.split('\t')
+		formated = line[1].replace("\"", "'")
+		print "gameforge[\"%s\"][%s] = \"%s\"\n" % (locale, line[0], formated.rsplit("\n", 1)[0])
+		fileOutput.write("gameforge[\"%s\"][%s] = \"%s\"\n" % (locale, line[0], formated.rsplit("\n", 1)[0]))
+
+	fileOutput.close()
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "USAGE: [locale]"
+		locale = raw_input("Enter locale name: ")
+		TranslateLocaleQuest(str(locale))
+
+	elif len(sys.argv) == 2:
+		TranslateLocaleQuest(sys.argv[1])
---
+++ LocaleStringBuilder.py
@@ -0,0 +1,69 @@
+__author__ = "Owsap"
+__copyright__ = "Copyright 2020, Owsap Productions"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+import os
+import sys
+import logging
+
+LOG_FILE_NAME = "LocaleStringBuilder.log" # Log file
+LOCALE_STRING_FILE = "locale_string.txt" # Locale strin file name
+LOCALE_STRING_BASE_FILE = "share/locale_string_vnum.txt" # Reference file name (String VNUM)
+
+logging.basicConfig(filename = "log/" + LOG_FILE_NAME, level = logging.DEBUG, format = '%(asctime)s %(message)s', datefmt = '%d/%m/%Y %H:%M:%S')
+
+def GetLocaleStringFile(locale):
+	return "locale/%s/%s" % (locale, LOCALE_STRING_FILE)
+
+def TransalteLocaleString(locale):
+	if not os.path.exists(LOCALE_STRING_BASE_FILE):
+		print "Reference file not found. %s" % LOCALE_STRING_BASE_FILE
+		logging.warning("Reference file not found. %s" % LOCALE_STRING_BASE_FILE)
+		return
+
+	localeStringOutput = "locale_string_%s.txt" % locale
+	if os.path.exists(localeStringOutput):
+		os.remove(localeStringOutput)
+
+	fileOutput = open(localeStringOutput, 'a')
+	for line in open(LOCALE_STRING_BASE_FILE, 'r'):
+		split = line.split('";')
+		vnum = split[0][1:]
+
+		if not vnum:
+			print ""
+			fileOutput.write("")
+
+		if not vnum.isdigit():
+			formated = split[0] + "\";"
+			print (formated.rsplit("\n", 1)[0])
+			fileOutput.write(formated.rsplit("\n")[0] + "\n")
+			continue
+
+		print GetTranslationVnum(locale, vnum)
+		fileOutput.write(GetTranslationVnum(locale, vnum) + "\n")
+
+	fileOutput.close()
+
+def GetTranslationVnum(locale, vnum):
+	lineCount = 0
+	for line in open(GetLocaleStringFile(locale), 'r'):
+		lineCount += 1
+		match = line.find(vnum)
+		if match == 0:
+			localeStringFile = open(GetLocaleStringFile(locale), 'r')
+			localeText = str(localeStringFile.readlines()[lineCount - 1])
+			split = localeText.split("\t")
+			formated = "\"" + split[1]
+
+			return (formated.rsplit("\n", 1)[0]) + "\";"
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "USAGE: [locale]"
+		locale = raw_input("Enter locale name: ")
+		TransalteLocaleString(str(locale))
+
+	elif len(sys.argv) == 2:
+		TransalteLocaleString(sys.argv[1])
